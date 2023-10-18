588379e40d8883838d3d837b67c47ef83e2cc7b4
Elany Peixoto <elanysantos5@outlook.com>
2023-10-18 15:07:13 -0300

    update

--- rotas.js
+++ rotas.js
@@ -9,7 +9,6 @@ rotas.get('/usuarios', usuarioController.buscar)
 rotas.get('/usuarios/:id', usuarioController.buscarPeloId)
 rotas.post('/usuarios', usuarioController.criar)
 rotas.post('/usuarios/login', usuarioController.logar)
-// aqui verifica o token - rotas.get('/usuarios', auth.verifyToken, usuarioController.buscar);
 rotas.get('/usuarios/:id/links', usuarioController.buscarLinkPeloId)
 rotas.post('/links/:id', auth.verifyToken, usuarioController.criarLink)
 rotas.put('/links/:id_link', auth.verifyToken, usuarioController.atualizarLink)
